ed75036399da71b7d25055a0c4c161983bfdcb10
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-10-11 14:27:38 +0300

    update validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
     belongs_to :user
     belongs_to :cocktail
+
+    validates :description, presence: true, length: {minimum: 50}
+    validates :rating, numericality: {in: 1..5}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :cocktails, though: :reviews
+
+    has_secure_password
+    validates :username, presence: true, uniqueness: true 
+    vaidates :name, presence: true
 end
