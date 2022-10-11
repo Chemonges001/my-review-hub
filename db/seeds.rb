e8ee250a9cc88c5805d1da9597070a2bc38bdc5a
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-10-11 14:45:14 +0300

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ u1 = User.create(name: "Oscar Kiprop", user_name: "The Legend" )
+ u2 = User.create(name: "Amos Koech", user_name: "Spiderman" )
+ u3 = User.create(name: "Susan Kamau", user_name: "Maya" )
+ u4 = User.create(name: "Tracey Lung'ahi", user_name: "Thiccky" )
+ u4 = User.create(name: "Titus Ouko", user_name: "Batman" )
